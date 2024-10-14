a80c42a40f5358d434fd886926aa626f32ad0feb
neslihannsrm <neslihannsrm@gmail.com>
2024-10-15 01:56:34 +0200

    added js index

---
+++ index.js
@@ -0,0 +1,43 @@
+
+console.log(`Hello World!`);
+
+let firstName = "Neslihan";
+let age = 21;
+let favColour = "blue";
+
+console.log(`My name is ${firstName}, I am ${age} years old and my favorite colour is ${favColour}.`);
+
+
+let books = 12
+console.log(`You have ${books} books.`);
+
+books -=10;
+console.log(`After removing 10 books, now you have ${books} books.`);
+
+books +=8;
+console.log(`After adding 8 books, now you have ${books} books.`);
+
+books *=2;
+console.log(`After doubling the number of books, now you have ${books} books.`);
+
+books /=5;
+console.log(`After dividing the number of books by 5, now you have ${books} books.`);
+
+console.log(`At the end you are left with ${books} books.`);
+
+let firstNumber = 15;
+let secondNumber = 3;
+
+// Perform arithmetic operations 
+let additionResult = firstNumber + secondNumber;
+let subtractionResult = firstNumber - secondNumber;
+let multiplicationResult = firstNumber * secondNumber;
+let divisionResult = firstNumber / secondNumber;
+
+// Log results 
+console.log(`After adding ${firstNumber} and ${secondNumber}, the result is: ${additionResult}`);
+console.log(`After subtracting ${secondNumber} from ${firstNumber}, the result is: ${subtractionResult}`);
+console.log(`After multiplying ${firstNumber} by ${secondNumber}, the result is: ${multiplicationResult}`);
+console.log(`After dividing ${firstNumber} by ${secondNumber}, the result is: ${divisionResult}`);
+
+
