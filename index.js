86280dcb545e6d05913baafdd3121bf235e1f0be
neslihannsrm <neslihannsrm@gmail.com>
2024-10-15 02:04:16 +0200

    added comments

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
 console.log(`Hello World!`);
 
+// Sentence with number and string
 let firstName = "Neslihan";
 let age = 21;
 let favColour = "blue";
 
 console.log(`My name is ${firstName}, I am ${age} years old and my favorite colour is ${favColour}.`);
 
-
+//One var example
 let books = 12
 console.log(`You have ${books} books.`);
 
+// Log results for one var example
 books -=10;
 console.log(`After removing 10 books, now you have ${books} books.`);
 
@@ -25,16 +27,17 @@ console.log(`After dividing the number of books by 5, now you have ${books} book
 
 console.log(`At the end you are left with ${books} books.`);
 
+//Two variable example
 let firstNumber = 15;
 let secondNumber = 3;
 
-// Perform arithmetic operations 
+// Perform arithmetic operations with 2 var example
 let additionResult = firstNumber + secondNumber;
 let subtractionResult = firstNumber - secondNumber;
 let multiplicationResult = firstNumber * secondNumber;
 let divisionResult = firstNumber / secondNumber;
 
-// Log results 
+// Log results for 2 var example
 console.log(`After adding ${firstNumber} and ${secondNumber}, the result is: ${additionResult}`);
 console.log(`After subtracting ${secondNumber} from ${firstNumber}, the result is: ${subtractionResult}`);
 console.log(`After multiplying ${firstNumber} by ${secondNumber}, the result is: ${multiplicationResult}`);
